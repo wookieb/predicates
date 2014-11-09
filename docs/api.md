6a81ba55b0b7342f5aae6373c0c1b0e73669c41e
Łukasz Kużyński 'wookieb <lukasz.kuzynski@gmail.com>
2014-11-09 18:06:45 +0100

    Better docs and examples for _structure_

--- docs/api.md
+++ docs/api.md
@@ -934,8 +934,16 @@ is.string({}); // false
 <a name="structure"></a>
 #structure(structure, [value])
 Checks whether an object satisfies predicates defined in structure
+
 NOTE: All predicates defined in structure must be satisfied.
 If some of the properties are optional use [undefinedOr](#undefinedOr)
+
+You shouldn't use this function to validate input from the user and expect complex report "what's wrong with this is object".
+There are few reasons for that:
+* it's just a predicate (that returns only true or false)
+* breaks [the design rule](design.md#user-content-defined-and-generated-predicates-will-not-throw-any-errors)
+
+See examples for inspiration how you can use _.structure
 
 **Params**
 
@@ -945,6 +953,7 @@ If some of the properties are optional use [undefinedOr](#undefinedOr)
 **Returns**: `Boolean` | [Predicate](#Predicate) - returns bool if more than 1 argument provided, otherwise a predicate  
 **Example**  
 ```js
+// simple object matching
 var is = require('predicates');
 
 var schema = {
@@ -959,6 +968,44 @@ isPerson(person); // true
 // same as
 is.structure(schema, person); // true
 isPerson({name: 'Tommy'});
+```
+**Example**  
+```js
+// filtering
+var is = require('predicates');
+
+var people = [
+ {name: 'Prof. Bend Ovah', age: 55, sex: 'male'},
+ {name: 'Dr. Supa Kaki', age: 34, sex: 'female'},
+ {name: 'Prof. Anti Santy', age: 46, sex: 'male'}
+];
+
+var professors = people.filter(is.structure({
+ name: is.startsWith('Prof.')
+}));
+
+// [
+//   {name: 'Prof. Bend Ovah', age: 55, sex: 'male'},
+//   {name: 'Prof. Anti Santy', age: 46, sex: 'male'}
+// ]
+```
+**Example**  
+```js
+// duck typing
+
+var isDuck = is.structure({
+ quack: is.function,
+ walk: is.function
+});
+
+isDuck({
+   say: function() { return 'woof! woof!';
+}}); // not a duck
+
+isDuck({
+   quack: function() { return 'quack!'; },
+   walk: function() { return 'tup tup tup'; }
+}); // yep, it's a duck
 ```
 <a name="true"></a>
 #true(value)
--- src/structure.js
+++ src/structure.js
@@ -9,12 +9,21 @@ var isObjectOfPredicates = objectOf(isFunction);
 
 /**
  * Checks whether an object satisfies predicates defined in structure
+ *
  * NOTE: All predicates defined in structure must be satisfied.
  * If some of the properties are optional use [undefinedOr](#undefinedOr)
+ *
+ * You shouldn't use this function to validate input from the user and expect complex report "what's wrong with this is object".
+ * There are few reasons for that:
+ * * it's just a predicate (that returns only true or false)
+ * * breaks [the design rule](design.md#user-content-defined-and-generated-predicates-will-not-throw-any-errors)
+ *
+ * See examples for inspiration how you can use _.structure
  *
  * @function structure
  *
  * @example
+ * // simple object matching
  * var is = require('predicates');
  *
  * var schema = {
@@ -29,6 +38,43 @@ var isObjectOfPredicates = objectOf(isFunction);
  * // same as
  * is.structure(schema, person); // true
  * isPerson({name: 'Tommy'});
+ *
+ * @example
+ * // filtering
+ * var is = require('predicates');
+ *
+ * var people = [
+ *  {name: 'Prof. Bend Ovah', age: 55, sex: 'male'},
+ *  {name: 'Dr. Supa Kaki', age: 34, sex: 'female'},
+ *  {name: 'Prof. Anti Santy', age: 46, sex: 'male'}
+ * ];
+ *
+ * var professors = people.filter(is.structure({
+ *  name: is.startsWith('Prof.')
+ * }));
+ *
+ * // [
+ * //   {name: 'Prof. Bend Ovah', age: 55, sex: 'male'},
+ * //   {name: 'Prof. Anti Santy', age: 46, sex: 'male'}
+ * // ]
+ *
+ * @example
+ * // duck typing
+ *
+ * var isDuck = is.structure({
+ *  quack: is.function,
+ *  walk: is.function
+ * });
+ *
+ * isDuck({
+ *    say: function() { return 'woof! woof!';
+ * }}); // not a duck
+ *
+ * isDuck({
+ *    quack: function() { return 'quack!'; },
+ *    walk: function() { return 'tup tup tup'; }
+ * }); // yep, it's a duck
+ *
  * @param {Object} structure
  * @param {Object} [value]
  * @return {(Boolean|Predicate)} returns bool if more than 1 argument provided, otherwise a predicate
